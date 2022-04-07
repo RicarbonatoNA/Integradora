43abc641b6851c9142e52d3693d0ba155644d252
Ricarbonato <101375487+RicarbonatoNA@users.noreply.github.com>
2022-04-06 20:17:53 -0500

    paulito

--- app/Http/Controllers/TemperaturaCont.php
+++ app/Http/Controllers/TemperaturaCont.php
@@ -10,7 +10,7 @@ class TemperaturaCont extends Controller
 {
     public function InsertarTemperatura(){ 
         
-        $response = Http::withHeaders(['X-AIO-Key' => env('ADAFRUIT_KEY')])
+        $response = Http::get(['X-AIO-Key' => env('ADAFRUIT_KEY')])
         ->get('https://io.adafruit.com/api/v2/juliomena1806/feeds/temperatura1/data/last');
         $value = $response['value'];
         $created_at = $response['created_at'];
